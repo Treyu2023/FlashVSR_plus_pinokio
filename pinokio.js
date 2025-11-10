a45e8c9128750c63b4e3f2c7f421d71723c230e6
Anchorite <ai_anchorite@proton.me>
2025-11-10 17:15:54 +1300

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,7 +1,7 @@
 const path = require('path')
 module.exports = {
   version: "3.7",
-  title: "FlashVSR_plus",
+  title: "FlashVSR_v0.5",
   description: "FlashVSR - Video Upscaler: [Runs on 12GB vram, 32GB ram] Diffusion-Based Streaming Video Super-Resolution",
   icon: "icon.png",
   menu: async (kernel, info) => {
